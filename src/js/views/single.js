72a63453574417da88af157d089f846c4dc07976
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-07-10 18:45:31 +0000

    tried and failed to get properties of character

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -7,13 +7,49 @@ export const Single = (props) => {
 	const { store, actions } = useContext(Context);
 	const { charId } = useParams();
 
-	const person = store.people.find(person => person.uid === charId);
+	const property = store.person.find(person => person.uid === charId);
+
+	useEffect(() =>{
+		fetch(`https://www.swapi.tech/api/people/${charId}`)
+		.then((result) => result.json())
+		.then((data) => actions.setPersonData(data.results));
+	}, []);
+
+	const { name, height, mass, eye_color, hair_color, birth_year, gender } =
+        personData;
 
 	return (
-		<div className="jumbotron">
-			<h1 className="display-4">{person.name}</h1>
+		<div className="container">
+			<div className="row">
+				<div className="w-65 d-flex flex-column justify-content-between">
+					<h1 className="display-4">{property.name}</h1>
+					<p className="lead mt-3 fs-3">
+						Lorem ipsum dolor sit amet consectetur adipisicing
+						elit. Asperiores itaque nesciunt suscipit alias
+						architecto voluptates fuga cupiditate, accusamus
+						exercitationem, expedita non perferendis error autem
+						neque reiciendis, minus iusto? Minima, natus.
+					</p>
+				</div>
+				<hr></hr>
+				<ul className="list-group d-flex justify-content-between mt-5">
+					<div className="d-flex flex-row justify-content-between">
+						<li className="list-group-item d-flex flex-column justify-content-between">
+							<div className="bold">Birth Year</div>
+							<div>{birth_year}</div>
+						</li>
+						<li className="list-group-item d-flex flex-column justify-content-between">
+							<div className="bold">Height</div>
+							<div>{height} cm.</div>
+						</li>
+						<li className="list-group-item d-flex flex-column justify-content-between">
+							<div className="bold">Mass</div>
+							<div>{mass} kg.</div>
+						</li>
+					</div>
+				</ul>
 
-			<hr className="my-4" />
+			</div>
 
 			<Link to="/">
 				<span className="btn btn-primary btn-lg" href="#" role="button">
